4c202ecbff4703d70fae7bb4a9a9227e066d6b3b
Khang Nguyen <dhoang.khang1504@gmail.com>
2024-10-18 06:01:01 +0700

    Delete Redundant Code

--- Assets/Scripts/Enemy/ObjectPool.cs
+++ Assets/Scripts/Enemy/ObjectPool.cs
@@ -5,42 +5,6 @@ using UnityEngine.Pool;
 
 public class ObjectPool : MonoBehaviour
 {
-    //public static ObjectPool SharedInstance;
-    //List<GameObject> pooledObjects;
-    //[SerializeField] GameObject objectToPool;
-    //[SerializeField] [Range(0, 50)] int amountToPool;
-
-    //void Awake()
-    //{
-    //    SharedInstance = this;
-    //    SpawnObject();
-    //}
-
-    //void SpawnObject()
-    //{
-    //    pooledObjects = new List<GameObject>();
-    //    GameObject tmp;
-    //    for (int i = 0; i < amountToPool; i++)
-    //    {
-    //        tmp = Instantiate(objectToPool, objectToPool.transform.position, Quaternion.identity, transform);
-    //        tmp.SetActive(false);
-    //        pooledObjects.Add(tmp);
-    //    }
-    //}
-
-    //public GameObject GetPooledObject()
-    //{
-    //    // Find and return the first object currently not active
-    //    for (int i = 0; i < amountToPool; i++)
-    //    {
-    //        if (!pooledObjects[i].activeInHierarchy)
-    //        {
-    //            return pooledObjects[i];
-    //        }
-    //    }
-    //    return null;
-    //}
-
     public static ObjectPool SharedInstance;
     
     [System.Serializable]
